188caa635905411ef0d7dadbbbd28f2ab4019222
Marv <bmurfield@gmail.com>
2024-10-20 23:18:26 -0400

    Gets server running

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -5,6 +5,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
+console.log(__dirname);
+// logging _dirname so that ts allows me to run this
 // TODO: Define route to serve index.html
 
 export default router;
